187b196f92cf0391b3a49450a0e6a4234c06cb6e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-07-06 23:54:40 +0600

    implemented user data table

---
+++ src/Components/DataTable/DataTable.js
@@ -0,0 +1,36 @@
+import { DataGrid } from "@mui/x-data-grid";
+import React from "react";
+import { userColumns, userRows } from "../../Pages/DataTableSource";
+import "./DataTable.scss";
+
+const DataTable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="dataTable">
+      <DataGrid
+        rows={userRows}
+        // columns={userColumns.concat(actionColumn)}
+        columns={[...userColumns, ...actionColumn]}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/Pages/List/List.js
+++ src/Pages/List/List.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import DataTable from '../../Components/DataTable/DataTable';
+import Navbar from '../../Components/Navbar/Navbar';
+import Sidebar from '../../Components/Sidebar/Sidebar';
 import "./List.scss"
 
 const List = () => {
     return (
-        <div>
-            <h2>List</h2>
+        <div className='list'>
+            <Sidebar/>
+            <div className="listContainer">
+                <Navbar/>
+                <DataTable/>
+            </div>
         </div>
     );
 };
